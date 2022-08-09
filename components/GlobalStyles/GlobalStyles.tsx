5178790080f3de288ad22ed99dbe4b34c5132c9f
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-08-09 22:45:30 +0200

    Refining

--- components/GlobalStyles/GlobalStyles.tsx
+++ components/GlobalStyles/GlobalStyles.tsx
@@ -1,22 +1,5 @@
 import { createGlobalStyle } from "styled-components"
-import COLORS from "@constants/colors"
-
-let colorVariables = "";
-
-for (const property in COLORS ) {
-  if(COLORS[property] instanceof Object) {
-    for(const shade in COLORS[property] as Object) {
-      if(shade === "default") {
-        colorVariables += `--${property}: ${Object(COLORS[property])[shade]};`
-      } else {
-        colorVariables += `--${property}-${shade}: ${Object(COLORS[property])[shade]};`
-      }
-    }
-  } else {
-    colorVariables += `--${property}: ${COLORS[property]};`
-  }
-  colorVariables += "\n"
-}
+import { colorVariables } from "./GlobalStyles.helpers"
 
 const GlobalStyles = createGlobalStyle`
   :root {
