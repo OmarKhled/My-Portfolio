0dd4133a57861187d43a70c5488e2b2c169cb3a9
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-09-11 18:24:36 +0200

    Previous Work Page

--- pages/previouswork.tsx
+++ pages/previouswork.tsx
@@ -1,10 +1,25 @@
+import ProjectsGrid from '@components/ProjectsGrid'
 import { NextPage } from 'next'
 import React from 'react'
+import styled from 'styled-components'
 
 const PreviousWork: NextPage = () => {
   return (
-    <div>PreviousWork</div>
+    <div>
+      <Section>
+        <SectionTitle>Projects</SectionTitle>
+        <ProjectsGrid />
+      </Section>
+    </div>
   )
 }
+
+const Section = styled.section`
+  margin: 5rem 0;
+  padding-bottom: 5rem;
+`
+const SectionTitle = styled.h4`
+  text-align: center;
+`
 
 export default PreviousWork
