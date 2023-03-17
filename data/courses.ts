a2223a9251103690ea9e90d22f94557357a79bf4
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2023-03-17 19:09:16 +0200

    Adjustments

--- data/courses.ts
+++ data/courses.ts
@@ -3,7 +3,7 @@ const slug = (name: string) => `/projects/${name}`;
 export default [
   {
     name: "Web Fundamentals",
-    avatarImg: "/images/courses/webfundamentals/logo.png",
+    avatarImg: "/images/courses/webfundamentals/logo.webp",
     description: "An introductory course for web technologies",
     link: slug("webfundamentals")
   },
