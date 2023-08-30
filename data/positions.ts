21009e7f0e708f2060e8a3d3289b7f01d2819bfe
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2023-08-30 17:50:44 +0300

    Edit Portfolio Description

--- data/positions.ts
+++ data/positions.ts
@@ -3,7 +3,7 @@ export default [
     name: "nichepharma",
     avatarImg: "/images/positions/niche.webp",
     description: "Frontend Engineer",
-    interval: "Jan 2022 - Present",
+    interval: "Jan 2023 - Present",
     link: "https://www.linkedin.com/company/nichepharma/"
   },
   
